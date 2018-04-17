f6b5670a56efd7c09cd8593b9e5e7bffe918878e
Hwan Seob Lee <bestseob93@gmail.com>
2018-04-17 17:07:14 +0900

    get a list of unlocked accounts from ganache test network server

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,3 +3,14 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3'); // constructor 함수는 대문자로 시작
 const web3 = new Web3(ganache.provider());
 
+beforeEach(() => {
+  web3.eth.getAccounts() // get a list of all accounts
+    .then(fetchedAccounts => {
+      console.log(fetchedAccounts);
+    });
+  // use one of those accounts to deploy the contract. 
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {});
+});
