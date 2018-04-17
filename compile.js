05e0497854c06bea3f2d34580222ca07596ede0f
Hwan Seob Lee <bestseob93@gmail.com>
2018-04-17 15:54:08 +0900

    mocha, ganache-cli, web3 added

--- compile.js
+++ compile.js
@@ -5,5 +5,4 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-console.log(solc.compile(source, 1));
-solc.compile(source, 1); // 2번째 인자는 컨트랙트의 갯수(현재는 1)
+module.exports = solc.compile(source, 1).contracts[':Inbox']; // 2번째 인자는 컨트랙트의 갯수(현재는 1)
