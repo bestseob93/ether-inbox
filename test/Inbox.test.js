b2f4c0f2dc7a60127cd30f716779317b5bc2a09a
Hwan Seob Lee <bestseob93@gmail.com>
2018-04-19 02:08:57 +0900

    async await code

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,14 +3,15 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3'); // constructor 함수는 대문자로 시작
 const web3 = new Web3(ganache.provider());
 
-beforeEach(() => {
-  web3.eth.getAccounts() // get a list of all accounts
-    .then(fetchedAccounts => {
-      console.log(fetchedAccounts);
-    });
+let accounts;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
   // use one of those accounts to deploy the contract. 
 });
 
 describe('Inbox', () => {
-  it('deploys a contract', () => {});
+  it('deploys a contract', () => {
+    console.log(accounts);
+  });
 });
